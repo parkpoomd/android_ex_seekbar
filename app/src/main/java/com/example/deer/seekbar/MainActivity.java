6431d2686ff8ca20451e2c3d09bada7fbb1bb75f
parkpoomd <parkpoom.d@gmail.com>
2018-12-03 13:46:55 +0700

    example seek bar simple

--- app/src/main/java/com/example/deer/seekbar/MainActivity.java
+++ app/src/main/java/com/example/deer/seekbar/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.deer.seekbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar mSeekBar;
+    private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeekBar = (SeekBar) findViewById(R.id.seek_bar);
+        mTextView = (TextView) findViewById(R.id.tv_progress_value);
+        mButton = (Button) findViewById(R.id.btn_show_progress_value);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mTextView.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText
+                        (MainActivity.this, String.valueOf(mSeekBar.getProgress()), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
